dc6fcceeae0ed5b109f5c832ddbb85400bb15696
jgfernandes <jgfernandes@users.noreply.github.com>
2023-10-19 01:26:10 -0300

    correct answers + feedback QuizGame.py
    only show correct answers and feedback after end questions in quiz.

--- QuizGame.py
+++ QuizGame.py
@@ -1,3 +1,5 @@
+# 1 QuizGame.py
+
 from DatabaseManager import DatabaseManager
 from QuizManager import QuizManager
 from QuestionManager import QuestionManager
@@ -7,8 +9,12 @@ class QuizGame:
         self.db_manager = db_manager
         self.quiz_manager = quiz_manager
         self.question_manager = question_manager
+        self.player_name = None  # Adicionamos um atributo para armazenar o nome do jogador
 
     def list_quizzes(self):
+        """
+        Lista todos os quizzes disponíveis no banco de dados.
+        """
         quizzes = self.db_manager.list_quizzes()
         if quizzes:
             print("\n--- Quizzes Disponíveis ---")
@@ -18,19 +24,29 @@ class QuizGame:
             print("Nenhum quiz encontrado.")
 
     def play_quiz(self, quiz_id):
+        """
+        Inicia o jogo para um quiz específico.
+        """
         if not self.db_manager.quizzes_table.contains(doc_id=quiz_id):
             print("Quiz não encontrado.")
             return
 
         quiz_data = self.db_manager.quizzes_table.get(doc_id=quiz_id)
         print(f"Jogando o Quiz: {quiz_data['nome']}")
-        questions = quiz_data.get('perguntas', [])  # Obtenha a lista de perguntas associadas a este quiz
+
+        if not self.player_name:
+            self.player_name = input("Digite seu nome: ")
+
+        questions = quiz_data.get('perguntas', [])
 
         if not questions:
             print("Este quiz não tem perguntas associadas.")
             return
 
         score = 0
+        total_questions = len(questions)
+        answers_summary = []  # Lista para armazenar respostas e feedbacks
+
         for question_id in questions:
             if not self.db_manager.questions_table.contains(doc_id=question_id):
                 print(f"Pergunta ID {question_id} não encontrada.")
@@ -41,24 +57,40 @@ class QuizGame:
             print(f"Título: {question_data['title']}")
             print(f"Texto: {question_data['text']}")
             options = question_data['options']
+            correct_answers = question_data['correct_answer']
+
             for i, option in enumerate(options):
                 print(f"{chr(65 + i)}. {option}")
 
-            user_answer = input("Escolha a letra correspondente à resposta correta: ").strip().upper()
+            user_answer = input("Escolha a letra ou número correspondente à resposta correta: ").strip().upper()
 
-            if user_answer.isalpha() and user_answer in [chr(65 + i) for i in range(len(options))]:
-                user_answer_index = ord(user_answer) - ord('A') + 1
-                correct_answers = question_data['correct_answer']
-                if user_answer_index in correct_answers:
-                    print("Resposta correta!")
-                    score += 1
-                else:
-                    print("Resposta incorreta.")
-            else:
-                print("Opção inválida. Você perdeu esta pergunta.")
+            while user_answer not in ['A', 'B', 'C', 'D', 'E', '1', '2', '3', '4', '5']:
+                print("Opção inválida. Por favor, escolha uma das letras ou números disponíveis.")
+                user_answer = input("Escolha a letra ou número correspondente à resposta correta: ").strip().upper()
 
-        print(f"Você respondeu corretamente a {score} de {len(questions)} perguntas.")
+            if user_answer.isnumeric():
+                user_answer_index = int(user_answer)
+                user_answer = chr(64 + user_answer_index)
 
+            user_answer_index = ord(user_answer) - ord('A') + 1
+            is_correct = user_answer_index in correct_answers
+            answers_summary.append({
+                'Título da Questão': question_data['title'],
+                'Resposta Correta': [chr(64 + ans) for ans in correct_answers],
+                'Resposta Fornecida': user_answer,
+            })
+
+            if is_correct:
+                score += 1
+
+        print(f"\nResumo das Respostas:")
+        for index, answer_summary in enumerate(answers_summary, 1):
+            print(f"Pergunta {index}:")
+            for key, value in answer_summary.items():
+                print(f"{key}: {value}")
+            print()
+
+        print(f"\n{self.player_name}, você respondeu corretamente a {score} de {total_questions} perguntas.")
 
     def manage_questions(self):
         self.question_manager.run()
@@ -93,7 +125,6 @@ class QuizGame:
 if __name__ == "__main__":
     db_manager = DatabaseManager()
     quiz_manager = QuizManager(db_manager)
-    question_manager = QuestionManager(db_manager)  # Passa o mesmo db_manager para QuestionManager
+    question_manager = QuestionManager(db_manager)
     game = QuizGame(db_manager, quiz_manager, question_manager)
     game.run()
-
